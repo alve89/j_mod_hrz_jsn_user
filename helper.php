b5f816f164946965bb5b6c3c38aee2fc95d95ad9
alve89 <devel@stefan-herzog.com>
2024-03-19 11:12:37 +0100

    Add function to store userdetails in user object

--- helper.php
+++ helper.php
@@ -8,4 +8,38 @@ class ModHrzShowSingleUserHelper {
 		echo '</pre>';
 		return;
     }
+	
+	public static function translateUserDetails(&$user, $userDetails) {
+
+		if(!is_null($userDetails)) {
+	
+			foreach($userDetails as $detail) {
+				switch($detail->name) {		
+					case 'profile-photo':
+						$user->imageFile = json_decode($detail->value)->imagefile;
+						if(substr($user->imageFile,0,1) != '/') {
+							$user->imageFile = '/' . $user->imageFile;
+						}
+						break;
+					case 'first-name':
+						$user->firstName = $detail->value;
+						break;
+					case 'last-name':
+						$user->lastName = $detail->value;
+					case 'convert-forms-contact-form-id':
+						$user->cfid = $detail->value;
+					case 'department':
+						$user->department = $detail->value;
+					case 'function':
+						$user->role = $detail->value;
+					case 'phone-number':
+						$user->phone = $detail->value;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+
 }
